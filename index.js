d725c87d65789fbdf43d646471ed6f71ef4f8bc1
titus njiru <njirutitus@gmail.com>
2020-10-14 14:04:48 +0300

    Number of options selected

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // main javascript file
 
-alert("Javascript file loaded");
+console.log("Javascript file loaded");
+
+function howMany(selectObject){
+    console.log(selectObject.tagName);
+    let numberSelected = 0;
+    for( let i = 0; i < selectObject.length; i++){
+        if(selectObject.options[i].selected){
+            numberSelected++;
+        }
+    }
+
+    return numberSelected;
+}
+
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click',function(){
+    result = document.getElementById("numberSelected");
+
+    result.innerHTML = "Number of options selected: " + howMany(document.selectForm.musicTypes);
+});
+
